961d0e50cb185166adfc06f2c75ac99ee42cbceb
arun-k-shrestha <kajiarun3@gmail.com>
2022-04-07 16:21:14 -0700

    changes

--- main.js
+++ main.js
@@ -1,17 +1,49 @@
+i = 0
 function background_color(id){
-    document.body.style.background = document.getElementById(id).innerHTML;
-    if (document.body.style.background == document.body.style.color){
-        document.body.style.background = "grey"
+    if (i == 0 && id == "black"){
         alert("Background color and text color can't be same")
+        document.body.style.background = "lightgrey"
+        sessionStorage.setItem("background", "lightgrey")
     }
+    
+    else{
+        document.body.style.background = document.getElementById(id).innerHTML;
+        sessionStorage.setItem("background", id)
+        if (document.body.style.background === document.body.style.color){
+            document.body.style.background = "lightgrey"
+            sessionStorage.setItem("background", "lightgrey")
+            alert("Background color and text color can't be same")
+        }
+    }
+
+    i =+ 1
 }
 
 
 function text_color(id){
-    document.body.style.color = document.getElementById(id).innerHTML;
-    if (document.body.style.background == document.body.style.color){
-        document.body.style.background = "grey"
-        alert("Background color and Text color can't be same")
-    
+
+    if (i == 0 && id == "grey"){
+        alert("Background color and text color can't be same")
+        document.body.style.background = "lightgrey"
+        sessionStorage.setItem("background", "lightgrey")
     }
+
+    else{
+        document.body.style.color = document.getElementById(id).innerHTML;
+        sessionStorage.setItem("text", id)
+        if (document.body.style.background === document.body.style.color){
+            document.body.style.background = "lightgrey"
+            sessionStorage.setItem("background", "lightgrey")
+            alert("Background color and Text color can't be same")
+        
+        }   
+    }
+
+    // sessionStorage.setItem("text", id)
+    i =+ 1
 }
+
+
+document.body.style.background = sessionStorage.getItem("background")
+document.body.style.color = sessionStorage.getItem("text")
+
