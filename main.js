a5e1cce2747a1a004178a0bb6757bc8396992176
arun-k-shrestha <kajiarun3@gmail.com>
2022-04-16 22:17:38 -0700

    changes

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+
+// These functions are for changing colors
 i = 0
 function background_color(id){
     if (i == 0 && id == "black"){
@@ -44,7 +46,8 @@ document.body.style.background = sessionStorage.getItem("background")
 document.body.style.color = sessionStorage.getItem("text")
 
 
-function OneCheckBox(checkbox){
+// these functions are for selecting only one checkbox
+function OneCheckBox1(checkbox){
     let checkboxes = document.getElementsByName('checkbox1')
     for(i=0;i<checkboxes.length; i++){
         if(checkboxes[i] !== checkbox){
@@ -53,7 +56,24 @@ function OneCheckBox(checkbox){
     }
 }
 
+function OneCheckBox2(checkbox){
+    let checkboxes = document.getElementsByName('checkbox2')
+    for(i=0;i<checkboxes.length; i++){
+        if(checkboxes[i] !== checkbox){
+            checkboxes[i].checked = false
+        }
+    }
+}
+
+
+// These function provides answers after hitting submit
+
 function submit1(){
+    let element1 = document.getElementById("checkbox1c")
+    element1.style.color = "lightgreen"
+
+    let element2 = document.getElementById("checkbox2b")
+    element2.style.color = "lightgreen"
+
     document.getElementById("answer").innerHTML = "The correct answers are: <br> <br> 1) Parentheses, Exponents, Multiplication or Division, Addition or Subtraction <br><br> 2) 7 "
     }
-
